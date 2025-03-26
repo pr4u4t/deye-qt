ade7ec2ff266b0009952a3c99e07cf37f8241ba3
root <root@a0d7b954-ssh.local.hass.io>
2025-03-26 19:00:01 +0100

    fixed parameters

--- deye-data-collector/src/main.cpp
+++ deye-data-collector/src/main.cpp
@@ -122,6 +122,10 @@ int main(int argc, char**argv){
     QJsonObject model;
     QTimer timer;
 
+    qDebug() << "***************************************************************";
+    qDebug() << "*                  STARTING INVERTER                          *";
+    qDebug() << "***************************************************************";
+
     QCoreApplication app(argc, argv);
     qSetMessagePattern("[%{time yyyyMMdd h:mm:ss.zzz t} %{if-debug}D%{endif}%{if-info}I%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}%{if-fatal}F%{endif}] %{file}:%{line} - %{message}");
 
